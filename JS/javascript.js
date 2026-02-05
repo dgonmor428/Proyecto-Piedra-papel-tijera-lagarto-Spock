7de1b5c33c766587e10ae95ef30da1f932d2b4e2
David González <dgonmor428@g.educaand.es>
2026-02-05 20:17:58 +0100

    Html completo y comienzo de JS

---
+++ JS/javascript.js
@@ -0,0 +1,30 @@
+// Constantes
+
+
+
+function inicializarJuego() {
+ 
+}
+function jugar(eleccionUsuario) {
+
+}
+function obtenerEleccionCPU() {
+
+}
+function mostrarEleccion(display, eleccion, jugador) {
+
+}
+function reiniciarDisplays() {
+
+}
+function calcularResultadoJugada(usuario, cpu) {
+
+}
+function mostrarResultadoJugada(resultado, usuario, cpu) {
+
+}
+function actualizarContadores() {
+
+}
+
+    
